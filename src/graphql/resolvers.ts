6d6a2f258ddf43309118e5e559885aedf68c051c
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-02-26 14:46:12 +1000

    Setup resolvers

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,10 +1,36 @@
 import { Cart, Product, User } from "@/types/graphql";
 import axios from "axios";
+import { GraphQLError } from "graphql";
 
 const FAKE_STORE_API = "https://fakestoreapi.com";
 
 export const resolvers = {
   Query: {
-  
+    // Fetch all products
+    products: async (): Promise<Product[]> => {
+      const { data } = await axios.get(`${FAKE_STORE_API}/products`);
+      return data;
+    },
+
+    // Fetch a product by ID
+    product: async (_: unknown, { id }: { id: string }): Promise<Product> => {
+      const { data } = await axios.get(`${FAKE_STORE_API}/products/${id}`);
+      return data;
+    },
+
+    // Fetch a user's cart by user ID
+    cart: async (
+      _: unknown,
+    ): Promise<Cart> => {
+      
+      // TODO: throw an error TO BE IMPLEMENTED
+      throw new GraphQLError("Not implemented");
+    },
+
+    // Fetch a user by ID
+    user: async (_: unknown, { id }: { id: string }): Promise<User> => {
+        // TODO: throw an error TO BE IMPLEMENTED
+        throw new GraphQLError("Not implemented");
+    },
   },
 };
