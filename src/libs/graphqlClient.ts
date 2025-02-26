4fdd591c47567109f4d912bd33086ca8bba96dc9
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-02-26 15:02:25 +1000

    Fixing graphql client

--- src/libs/graphqlClient.ts
+++ src/libs/graphqlClient.ts
@@ -1,11 +1,15 @@
-import { request as graphqlRequest, RequestDocument, Variables } from 'graphql-request'
+import {
+  request as graphqlRequest,
+  RequestDocument,
+  Variables
+} from "graphql-request";
 
-const dev = process.env.NODE_ENV !== 'production'
-export const API_URL = dev
-  ? process.env.NEXT_PUBLIC_API_URL
-  : 'https://...'
+export const API_URL = "http://localhost:3000/api/graphql";
 
-export const request = async <T = any, V = Variables>(document: RequestDocument, variables?: V): Promise<T> => {
-  // @ts-ignore: temporary ignore the typings
-  return await graphqlRequest<T, V>(API_URL, document, variables)
-}
+export const request = async <T = unknown, V = Variables>(
+  document: RequestDocument,
+  variables?: V
+): Promise<T> => {
+  // @ts-expect-error: temporary ignore the typings
+  return await graphqlRequest<T, V>(API_URL, document, variables);
+};
