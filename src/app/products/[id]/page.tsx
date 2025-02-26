6e61ffb1302fa724f50215becbf294d7605a89b3
Vinicius Silva <viniciuspsilvas@gmail.com>
2025-02-26 17:28:58 +1000

    Setup SEO

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import ProductDetails from "@/app/components/ProductDetails";
 
 interface ProductDetailProps {
@@ -6,8 +7,36 @@ interface ProductDetailProps {
   };
 }
 
+// Fetch product details for metadata
+// TODO We can improve it getting the data from the graphql API instead
+async function getProduct(id: string) {
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  return response.json();
+}
+
+export async function generateMetadata({
+  params
+}: ProductDetailProps): Promise<Metadata> {
+  const product = await getProduct(params.id);
+
+  return {
+    title: `${product.title} - My Store`,
+    description: product.description,
+    openGraph: {
+      type: "website",
+      title: product.title,
+      description: product.description,
+      images: [{ url: product.image }],
+      url: `https://yourstore.com/products/${params.id}`
+    },
+    other: {
+      "og:type": "product"
+    }
+  };
+}
+
 export default async function Page({ params }: ProductDetailProps) {
-   // asynchronous access of `params.id`.
+  // asynchronous access of `params.id`.
   const { id } = await params;
   return (
     <main>
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,16 @@
 import ProductList from "../components/ProductList";
 
+export const metadata = {
+  title: "Products - My Store",
+  description: "Browse our amazing collection of products.",
+  openGraph: {
+    title: "Products - My Store",
+    description: "Browse our amazing collection of products.",
+    images: ["/public/globe.svg"],
+    type: "website",
+  },
+};
+
 export default function Products() {
   return (
     <main>
